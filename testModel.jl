a4187025c94405f4ad69b39696145de7145f8fae
GenaBitu <genabitu@gmail.com>
2016-12-09 15:21:58 +0100

    Own test function

--- testModel.jl
+++ testModel.jl
@@ -2,20 +2,20 @@ push!(LOAD_PATH, "EduNets/src");
 
 using EduNets
 
-function testModel(model::SingleBagModel, dataset::SingleBagDataset; T::DataType=Float32, lambda::Float32=1f-6, iter::Int=1000)
-  sc=ScalingLayer(dataset.x);
-  g=deepcopy(model);
-  gg=model2vector(model);
+function threshold(input::StridedArray{Float32}, threshold::Float32)::Array{Int64}
+	output = Array{Int64, 1}(length(input));
+	for i in 1:length(input)
+		if(input[i] < threshold)
+			output[i] = 1;
+		else
+			output[i] = 2;
+		end
+	end
+	return output;
+end
 
-  function optFun(x::Vector)
-    update!(model,x);
-    dss=sample(dataset,[10,10]);
-    f=gradient!(model,dss,g);
-    f+=l1regularize!(model,g, T(lambda));
-    model2vector!(g,gg);
-    return(f,gg);
-  end
-
-  theta=model2vector(model);
-	return testgradient(optFun, theta; verbose=1);
+function testModel(model::SingleBagModel, dataset::SingleBagDataset)
+	out = forward!(model, dataset);
+	clamped = threshold(out, 0.5f0);
+	return clamped;
 end
