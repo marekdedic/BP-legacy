bf680354156429c812eb63a9dac9ae0fbfddd465
GenaBitu <genabitu@gmail.com>
2016-12-18 00:02:44 +0100

    PR Curve, albeit slow

--- testModel.jl
+++ testModel.jl
@@ -77,44 +77,37 @@ function ROCValue(T::Float32, predicted::StridedArray{Float32}, real::Array{Int6
 end
 
 function testModelROCCustom(model::SingleBagModel, dataset::SingleBagDataset)
-	modC = deepcopy(model);
-	datC = deepcopy(dataset);
-	out = forward!(modC, datC);
-	samples = 100;
-	pmask=dataset.y.==2
-	nmask=dataset.y.==1
-	thresholds=sort(out[nmask],rev=true)
-	TPR=zeros(sum(nmask))
-	FPR=zeros(sum(nmask))
-	for (i,th) in enumerate(thresholds)
-		FPR[i]=mean(out[nmask].>th)
-		TPR[i]=mean(out[pmask].>th)
+	out = forward!(model, dataset);
+	pmask = dataset.y .== 2; # Bool array, true when dataset.y == 2
+	nmask = dataset.y .== 1;
+	thresholds = sort(out[nmask], rev = true);
+	TPR = zeros(sum(nmask)); # Zero array with length equal to number of real negatives
+	FPR = zeros(sum(nmask));
+	for (i,it) in enumerate(thresholds)
+		FPR[i] = mean(out[nmask] .> it); # Mean over Bool array (percentage of true values), true when truly negative but prediction higher then threshold
+		TPR[i] = mean(out[pmask] .> it);
 	end
-	println(hcat(TPR, FPR));
-	plot(FPR,TPR);
+	plot(FPR, TPR; xlabel = "False-positive rate", ylabel = "True-positive rate");
 end
 
 function testModelROC(model::SingleBagModel, dataset::SingleBagDataset)
-	modC = deepcopy(model);
-	datC = deepcopy(dataset);
-	out = forward!(modC, datC);
-	(target, nontarget) = separate(out, datC.y);
+	out = forward!(model, dataset);
+	(target, nontarget) = separate(out, dataset.y);
 	rocPlot = roc(target, nontarget)
 	Winston.plot(rocPlot);
 end
 
 function testModelPR(model::SingleBagModel, dataset::SingleBagDataset)
-	modC = deepcopy(model);
-	datC = deepcopy(dataset);
-	out = forward!(modC, datC);
-	samples = 100;
-	P = Array{Float32, 1}(samples);
-	R = Array{Float32, 1}(samples);
-	for i in 1:samples
-		clamped = threshold(out, Float32(i / samples));
-		(precision, recall) = PR(clamped, datC.y);
-		push!(P, precision);
-		push!(R, recall);
+	out = forward!(model, dataset);
+	pmask = dataset.y .== 2; # Bool array, true when dataset.y == 2
+	nmask = dataset.y .== 1;
+	thresholds = sort(out[nmask], rev = true);
+	Precision = zeros(sum(nmask)); # Zero array with length equal to number of real negatives
+	Recall = zeros(sum(nmask));
+	for (i, it) in enumerate(thresholds)
+		ppmask = out .> it;
+		Precision[i] = mean(dataset.y[ppmask] .== 2)
+		Recall[i] = mean(out[pmask] .> it);
 	end
-	plot(P, R);
+	plot(Recall, Precision; xlabel = "Recall", ylabel = "Precision");
 end
