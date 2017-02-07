f8d461fbddd9cb55119c9c9f5f68f69e56eca5d1
GenaBitu <genabitu@gmail.com>
2017-02-07 10:26:19 +0100

    Fix for newer version of EduNets

--- initializeModel.jl
+++ initializeModel.jl
@@ -14,7 +14,7 @@ function initializeModel(;file::AbstractString="dataset.jld", percentage::Float3
 	numTrain = Int(round(length(indices) * percentage));
 	trainDataset = dataset[indices[1:numTrain]];
 	testDataset = dataset[(numTrain + 1):length(indices)];
-	model = SingleBagModel(StackedBlocks(ReluLayer((size(trainDataset.x, 1), insideLayers); T=Float32), ReluLayer((insideLayers, insideLayers); T=Float32);T=Float32), MeanPoolingLayer(insideLayers; T=Float32), LinearLayer((insideLayers, 1); T=Float32), HingeLoss(; T=Float32); T=Float32);
+	model = SingleBagModel(ReluLayer((size(trainDataset.x, 1), insideLayers);T=Float32), MeanPoolingLayer(insideLayers; T=Float32), LinearLayer((insideLayers, 1); T=Float32), HingeLoss(; T=Float32); T=Float32);
 	return (model, trainDataset, testDataset);
 end
 
--- main.jl
+++ main.jl
@@ -2,7 +2,7 @@ push!(LOAD_PATH, "EduNets/src");
 
 include("include.jl");
 
-(model, trainDataset, testDataset) = initializeModel(file = "dataset.jld", percentage = 0.86f0);
+(model, trainDataset, testDataset) = initializeModel(file = "datasetFull.jld", percentage = 0.86f0);
 trainModel!(model, trainDataset);
 
 #testModelROC(model, testDataset);
