245fedd60f08e8b0309b897616823b5b59b4ae78
GenaBitu <genabitu@gmail.com>
2017-02-09 15:54:07 +0100

    Fixed model dimensions

--- main.jl
+++ main.jl
@@ -2,7 +2,7 @@ push!(LOAD_PATH, "EduNets/src");
 
 include("include.jl");
 
-(model, loss, trainDataset, testDataset) = initializeModelUrl(file = "datasetFullUrl.jld", percentage = 0.86f0);
+(model, loss, trainDataset, testDataset) = initializeModelUrl(file = "dataset.jld", percentage = 0.86f0);
 trainModelUrl!(model, loss, trainDataset);
 
 #testModelROC(model, testDataset);
