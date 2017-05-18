c8e90cdd7a294c5860cfdce723316ef3af110eb1
GenaBitu <genabitu@gmail.com>
2017-05-18 23:14:36 +0200

    Removed parseDataset

--- include.jl
+++
@@ -1,9 +0,0 @@
-include("UrlDataset.jl");
-include("UrlModel.jl");
-
-include("loadDataset.jl");
-include("initializeModel.jl");
-include("initializeModelUrl.jl");
-include("trainModel.jl");
-include("trainModelUrl.jl");
-
--- labellers.jl
+++ labellers.jl
@@ -1,7 +1,8 @@
 "Sets up a global dictionary for AVClassLabeller, so that it doesn't have to be loaded every time."
-function setupAVClass(file::AbstractString)
+function setupAVClass()
 	global avclass_dict = Dict{String, String}();
-	open(file) do file
+	run(`./avclass_proccess.sh`);
+	open("avclass_results.txt") do file
 		for line in eachline(file)
 			values = split(chomp(line), '\t');
 			if size(values)[1] > 2
@@ -9,57 +10,7 @@ function setupAVClass(file::AbstractString)
 			end
 		end
 	end
-end
-
-function countingLabeller(file::AbstractString; threshold::Int = 5)::Int
-	positiveCount = 0;
-	open(file) do f
-		json = JSON.parse(f);
-		try
-			scans = json["scans"];
-			try
-				if(scans["Malwarebytes"]["detected"] == true)
-					positiveCount += 1;
-				end
-			end
-			try
-				if(scans["BitDefender"]["detected"] == true)
-					positiveCount += 1;
-				end
-			end  
-			try
-				if(scans["Symantec"]["detected"] == true)
-					positiveCount += 1;
-				end
-			end
-			try
-				if(scans["ESET-NOD32"]["detected"] == true)
-					positiveCount += 1;
-				end
-			end
-			try
-				if(scans["Avast"]["detected"] == true)
-					positiveCount += 1;
-				end
-			end
-			try
-				if(scans["Kaspersky"]["detected"] == true)
-					positiveCount += 1;
-				end
-			end
-			try
-				if(scans["Avira"]["detected"] == true)
-					positiveCount += 1;
-				end
-			end
-			try
-				if(scans["AVG"]["detected"] == true)
-					positiveCount += 1;
-				end
-			end
-		end
-	end
-	return positiveCount >= threshold ? 2 : 1;
+	run(`./avclass_results.txt`);
 end
 
 function AVClassLabeller(file::AbstractString)::Int
